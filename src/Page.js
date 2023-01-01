c5ba09ccc1c1b58bdf80081689fb775b0b3faff2
unknown <prariya63@gmail.com>
2023-01-01 18:24:09 +0700

    first commit

---
+++ src/Page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Page.css';
+
+function Page(){
+    return(
+       <div className="page">
+            <div className="top-bar">
+                <p>Deliver to:</p>
+            </div>
+       </div>
+    )
+}
+
+export default Page;
